d2a2cd50cc6acfe56ad47556a7102729b03d1ab7
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-02 11:12:50 +0700

    fig bug validate <a>

--- dhd_web_client/src/pages/Home/BreadcrumbExample.js
+++ dhd_web_client/src/pages/Home/BreadcrumbExample.js
@@ -2,42 +2,30 @@ import React from 'react';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import styles from './Home.module.scss'
 import classNames from "classnames/bind";
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 
 const cx = classNames.bind(styles)
 
 function BreadcrumbExample({movie}) {
+    const navigate = useNavigate()
+    const returnHome = () => {
+        navigate('/')
+    }
+
     return (
         <>
-            {
-                movie ? (
-                    <Breadcrumb className={cx('breadcrumb')}>
-                        <Breadcrumb.Item>
-                            <NavLink to={'/'}>
-                                Home
-                            </NavLink>
-                        </Breadcrumb.Item>
-                        <Breadcrumb.Item
-                            href="/"
-                            active
-                        >
-                            Movie
-                        </Breadcrumb.Item>
-                        <Breadcrumb.Item
-                            href="/"
-                            active
-                        >
-                            {movie.title}
-                        </Breadcrumb.Item>
-                    </Breadcrumb>
-                ) : (
-                    <Breadcrumb className={cx('breadcrumb')}>
-                        <Breadcrumb.Item>
-                            Home
-                        </Breadcrumb.Item>
-                    </Breadcrumb>
-                )
-            }
+            <Breadcrumb className={cx("breadcrumb")}>
+                <Breadcrumb.Item>
+                    <div onClick={returnHome}>Home</div>
+                    {/*<NavLink to="/">Home</NavLink>*/}
+                </Breadcrumb.Item>
+                {movie ? (
+                    <>
+                        <Breadcrumb.Item active>Movie</Breadcrumb.Item>
+                        <Breadcrumb.Item active>{movie.title}</Breadcrumb.Item>
+                    </>
+                ) : null}
+            </Breadcrumb>
         </>
     )
 }
