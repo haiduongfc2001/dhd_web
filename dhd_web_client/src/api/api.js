80ff0da78899a50122d3bfe2555a15350fef6f89
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-15 23:06:49 +0700

    Update api.js

--- dhd_web_client/src/api/api.js
+++ dhd_web_client/src/api/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:5000', // Đặt địa chỉ cơ sở của backend
+  baseURL: "http://localhost:5000", // Đặt địa chỉ cơ sở của backend
+  // baseURL: "https://dhd-admin-server.vercel.app",
 });
 
 export default instance;
