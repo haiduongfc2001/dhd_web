aad1aa5b62768a0490d123e342daa4a0bbffdf15
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-15 23:09:35 +0700

    remove test.js

--- dhd_web_client/src/routes/index.js
+++ dhd_web_client/src/routes/index.js
@@ -8,30 +8,28 @@ import ResetPassword from "~/components/Authentication/ForgotPassword/ResetPassw
 import EmailVerified from "~/components/Authentication/EmailVerified";
 import Profile from "~/pages/Profile/Profile";
 import MovieInfo from "~/pages/MovieInfo/MovieInfo";
-import Test from "~/test";
 import SearchMovies from "~/pages/SearchMovies/SearchMovies";
 
 export const publicRoutes = [
-    { path: '/register', component: Register, layout: null },
-    { path: '/login', component: SignIn, layout: null },
-    // { path: '/test', component: Test, layout: null },
-    { path: '/not-found', component: NotFound },
-    { path: '/', component: Home },
-    { path: '/verify', component: EmailVerified, layout: null },
-    { path: '/forgot-password', component: ForgotPassword, layout: null },
-    { path: '/reset-password', component: ResetPassword, layout: null },
-    { path: '/profile', component: Profile },
-    { path: '/movie/:_id', component: MovieInfo },
-    { path: '/search', component: SearchMovies }
-]
+  { path: "/register", component: Register, layout: null },
+  { path: "/login", component: SignIn, layout: null },
+  // { path: '/test', component: Test, layout: null },
+  { path: "/not-found", component: NotFound },
+  { path: "/", component: Home },
+  { path: "/verify", component: EmailVerified, layout: null },
+  { path: "/forgot-password", component: ForgotPassword, layout: null },
+  { path: "/reset-password", component: ResetPassword, layout: null },
+  { path: "/profile", component: Profile },
+  { path: "/movie/:_id", component: MovieInfo },
+  { path: "/search", component: SearchMovies },
+];
 
 // Private Routes
-export const privateRoutes = [
-]
+export const privateRoutes = [];
 
 export const errorRoutes = [
-    // { path: '/not-found', component: NotFound },
-    // ... other error routes
+  // { path: '/not-found', component: NotFound },
+  // ... other error routes
 ];
 
 // export  default [
