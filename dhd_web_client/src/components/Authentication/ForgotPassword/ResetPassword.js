1b859ec7cf99c053949fa9b95a302f2233c95fd9
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-27 23:22:44 +0700

    validate password

--- dhd_web_client/src/components/Authentication/ForgotPassword/ResetPassword.js
+++ dhd_web_client/src/components/Authentication/ForgotPassword/ResetPassword.js
@@ -1,150 +1,169 @@
-import React, {useEffect, useState} from "react";
-import {NavLink, useNavigate, useParams} from "react-router-dom";
-import {toast, ToastContainer} from "react-toastify";
-import {MDBBtn, MDBCard, MDBCardBody, MDBCol, MDBContainer, MDBInput, MDBRow} from "mdb-react-ui-kit";
+import React, { useEffect, useState } from "react";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBInput,
+  MDBRow,
+} from "mdb-react-ui-kit";
 
 import classNames from "classnames/bind";
 import styles from "../Authentication.module.scss";
 import api from "~/api/api";
 import axios from "axios";
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 const ResetPassword = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [notificationMessage, setNotificationMessage] = useState("");
 
-    const [errorMessage, setErrorMessage] = useState('');
-    const [notificationMessage, setNotificationMessage] = useState('');
+  const [user_id, setUser_id] = useState("");
 
-    const [user_id, setUser_id] = useState('');
+  useEffect(() => {
+    const token = new URLSearchParams(window.location.search).get("token");
+    api
+      .get(`/forget-password?token=${token}`)
+      .then((response) => {
+        setUser_id(response.data.user_id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        const token = new URLSearchParams(window.location.search).get('token');
-        api.get(`/forget-password?token=${token}`)
-            .then(response => {
-                setUser_id(response.data.user_id);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, []);
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
 
-    const handleResetPassword = async (e) => {
-        e.preventDefault();
-
-        if (!password || !confirmPassword) {
-            setErrorMessage("Xin hãy nhập mật khẩu!");
-            return; // Dừng lại nếu không nhập password
-        }
-        if (password !== confirmPassword) {
-            setErrorMessage("Mật khẩu không khớp");
-            return;
-        }
-
-        try {
-
-            const response = await api.post('/forget-password', {
-                password: password,
-                user_id: user_id,
-            });
-            setErrorMessage('');
-            setNotificationMessage(response.data.message);
-
-        } catch (error) {
-            if (error.response && error.response.data && error.response.data.message) {
-                setErrorMessage(error.response.data.message);
-            } else {
-                setErrorMessage("Lỗi server");
-            }
-        }
+    if (!password || !confirmPassword) {
+      setErrorMessage("Xin hãy nhập mật khẩu!");
+      return; // Dừng lại nếu không nhập password
     }
 
-    return (
-        <div className={cx('wrapper')}>
-            <MDBContainer fluid className={cx('signin-form')}>
-                <MDBRow className='d-flex justify-content-center align-items-center h-100'>
-                    <MDBCol col='12'>
-                        <MDBCard className='bg-light text-black my-5 mx-auto'
-                                 style={{borderRadius: '1rem', maxWidth: '400px'}}>
-                            <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
-                                <p className="text-black-150 mt-3 mb-4">
-                                    Đặt lại mật khẩu
-                                </p>
+    // Validate password: mật khẩu phải có ít nhất 6 kí tự, gồm chữ hoa chữ thường và số
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
+    if (!password.match(passwordRegex)) {
+      setErrorMessage(
+        "Mật khẩu phải chứa ít nhất 6 ký tự, bao gồm chữ hoa, chữ thường và số."
+      );
+      return;
+    }
 
-                                {/*<h1>User ID: {user_id}</h1>*/}
+    if (password !== confirmPassword) {
+      setErrorMessage("Mật khẩu không khớp");
+      return;
+    }
 
-                                <MDBInput
-                                    wrapperClass='mb-4 mx-5 w-100'
-                                    labelClass='text-black'
-                                    label={
-                                        <>
-                                            Nhập mật khẩu mới{" "}
-                                            <span
-                                                style={{color: "red"}}
-                                                dangerouslySetInnerHTML={{__html: "*"}}
-                                            />
-                                        </>}
-                                    type='password'
-                                    size='lg'
-                                    style={{maxWidth: '250px'}}
-                                    autoFocus
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                                <MDBInput
-                                    wrapperClass='mb-4 mx-5 w-100'
-                                    labelClass='text-black'
-                                    label={
-                                        <>
-                                            Xác nhận mật khẩu mới{" "}
-                                            <span
-                                                style={{color: "red"}}
-                                                dangerouslySetInnerHTML={{__html: "*"}}
-                                            />
-                                        </>}
-                                    type='password'
-                                    size='lg'
-                                    style={{maxWidth: '250px'}}
-                                    value={confirmPassword}
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
-                                />
+    try {
+      const response = await api.post("/forget-password", {
+        password: password,
+        user_id: user_id,
+      });
+      setErrorMessage("");
+      setNotificationMessage(response.data.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        setErrorMessage("Lỗi server");
+      }
+    }
+  };
 
-                                {errorMessage && (
-                                    <p className={"text-danger"}>{errorMessage}</p>
-                                )}
-                                {notificationMessage && (
-                                    <p className={"text-success"}>{notificationMessage}</p>
-                                )}
+  return (
+    <div className={cx("wrapper")}>
+      <MDBContainer fluid className={cx("signin-form")}>
+        <MDBRow className="d-flex justify-content-center align-items-center h-100">
+          <MDBCol col="12">
+            <MDBCard
+              className="bg-light text-black my-5 mx-auto"
+              style={{ borderRadius: "1rem", maxWidth: "400px" }}
+            >
+              <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+                <p className="text-black-150 mt-3 mb-4">Đặt lại mật khẩu</p>
 
-                                <MDBBtn
-                                    className='mx-2 px-5 text-black'
-                                    color='red'
-                                    size='lg'
-                                    style={{backgroundColor: '#a69c9c', fontWeight: '600'}}
-                                    onClick={handleResetPassword}
-                                >
-                                    Xác nhận
-                                </MDBBtn>
+                {/*<h1>User ID: {user_id}</h1>*/}
 
-                                <p className='small mt-3 mb-3 pb-lg-2'>
-                                    <NavLink
-                                        className='text-black-100'
-                                        to='/login'
-                                    >
-                                        Đăng nhập
-                                    </NavLink>
-                                </p>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
+                <MDBInput
+                  wrapperClass="mb-4 mx-5 w-100"
+                  labelClass="text-black"
+                  label={
+                    <>
+                      Nhập mật khẩu mới{" "}
+                      <span
+                        style={{ color: "red" }}
+                        dangerouslySetInnerHTML={{ __html: "*" }}
+                      />
+                    </>
+                  }
+                  type="password"
+                  size="lg"
+                  style={{ maxWidth: "250px" }}
+                  autoFocus
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <MDBInput
+                  wrapperClass="mb-4 mx-5 w-100"
+                  labelClass="text-black"
+                  label={
+                    <>
+                      Xác nhận mật khẩu mới{" "}
+                      <span
+                        style={{ color: "red" }}
+                        dangerouslySetInnerHTML={{ __html: "*" }}
+                      />
+                    </>
+                  }
+                  type="password"
+                  size="lg"
+                  style={{ maxWidth: "250px" }}
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
 
-            <ToastContainer/>
-        </div>
-    );
-}
+                {errorMessage && (
+                  <p className={"text-danger"}>{errorMessage}</p>
+                )}
+                {notificationMessage && (
+                  <p className={"text-success"}>{notificationMessage}</p>
+                )}
+
+                <MDBBtn
+                  className="mx-2 px-5 text-black"
+                  color="red"
+                  size="lg"
+                  style={{ backgroundColor: "#a69c9c", fontWeight: "600" }}
+                  onClick={handleResetPassword}
+                >
+                  Xác nhận
+                </MDBBtn>
+
+                <p className="small mt-3 mb-3 pb-lg-2">
+                  <NavLink className="text-black-100" to="/login">
+                    Đăng nhập
+                  </NavLink>
+                </p>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+
+      <ToastContainer />
+    </div>
+  );
+};
 
 export default ResetPassword;
